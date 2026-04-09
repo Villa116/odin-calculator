498254c5bd453194d8b9c90d98b86bbfc9c5d890
Villa116 <66364019+Villa116@users.noreply.github.com>
2026-04-09 16:36:42 +0200

    Add functionality to perform basic math operations
    handle how to select a digit and operator on the calculator.
    
    Implement function to perform calculation and display result.

--- script.js
+++ script.js
@@ -1,50 +1,119 @@
-const firstNumber = 0;
-const secondNumber = 0;
-const operator = "";
-let result;
+let firstOperand = null;
+let secondOperand = null;
+let currentOperator = null;
+let displayValue = '0';
+let waitingForSecondOperand = false;
 
+const container = document.querySelector('.container');
+const display = document.querySelector('.display');
 
+container.addEventListener('click', (e) => {
+  const { target } = e;
 
-function add(a, b) {
-  return a + b;
-}
+  if(!target.classList.contains('btn')) return;
 
-function substract(a, b) {
-  return a - b;
-}
+  const { number, action } = target.dataset;
 
-function multiply(a, b) {
-  return a * b;
-}
+  if (number !== undefined) {
+    handleNumber(number);
+  } else {
+    handleAction(action);
+  }
 
-function divide(a, b) {
-  return a / b;
-}
+  display.innerText = displayValue;
+});
 
-function operate(operator, firstNum, secondNum) {
-  switch (operator) {
-    case "+":
-      result = add(firstNum, secondNum);
+function handleAction(action) {
+  switch (action) {
+    case 'clear':
+      displayValue = '0';
+      firstOperand = null;
+      secondOperand = null;
+      currentOperator = null;
       break;
 
-    case "-":
-      result = substract(firstNum, secondNum);
+    case 'decimal':
+      if (waitingForSecondOperand) {
+        displayValue = '0.';
+        waitingForSecondOperand = false;
+        break;
+      }
+
+      if(!displayValue.includes('.')) {
+        displayValue += '.';
+      } 
       break;
 
-    case "*":
-      result = multiply(firstNum, secondNum);
+    case 'add':
+    case 'subtract':
+    case 'multiply':
+    case 'divide':
+      handleOperator(action);
       break;
 
-    case "/":
-      result = divide(firstNum, secondNum);
-      break;
-  
-    default:
-      result = 0;
-      break;
+    case 'calculate':
+      if (currentOperator && firstOperand !== null) {
+        secondOperand = displayValue;
+        displayValue = String(operate(currentOperator, firstOperand, secondOperand));
+
+        firstOperand = null;
+        currentOperator = null;
+      }
+        break;
   }
 }
 
-operate("/", 10, 4);
+function handleNumber(num) {
+  if (waitingForSecondOperand) {
+    displayValue = num;
+    waitingForSecondOperand = false;
+    return;
+  }
 
-console.log(result);
+  if (displayValue === '0') {
+    displayValue = num;
+  } else {
+    displayValue += num;
+  }
+}
+
+function handleOperator(nextOperator) {
+  firstOperand = displayValue;
+  currentOperator = nextOperator;
+  waitingForSecondOperand = true;
+}
+
+
+function add(a, b) { return a + b; }
+
+function subtract(a, b) { return a - b; }
+
+function multiply(a, b) { return a * b; }
+
+function divide(a, b) {
+  return b === 0 ? "division by 0 not allowed" : a / b;
+}
+
+function operate(action, a, b) {
+  const firstNum = Number(a);
+  const secondNum = Number(b);
+
+  switch (action) {
+    case 'add':
+      return add(firstNum, secondNum);
+
+    case 'subtract':
+      return subtract(firstNum, secondNum);
+
+    case 'multiply':
+      return multiply(firstNum, secondNum);
+
+    case 'divide':
+      if(secondNum === 0) return "LOL NO"
+      return divide(firstNum, secondNum);
+  
+    default:
+      return "Error";
+  }
+}
+
